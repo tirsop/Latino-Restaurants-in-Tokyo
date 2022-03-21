dc92a7102c6f1f6e0c41051f3685a1ea7a634414
Tirso Pascual <tpascualgarcia@gmail.com>
2022-03-21 18:26:35 +0900

    simplify the restaurant model

--- models/restaurant.js
+++ models/restaurant.js
@@ -2,10 +2,9 @@ import mongoose from "mongoose";
 
 const RestaurantSchema = new mongoose.Schema({
     title: String,
-    price: Number,
-    description: String,
     location: String,
-    image: String
+    image: String,
+    url: String
 })
 
 export default mongoose.model('Restaurant', RestaurantSchema); 
