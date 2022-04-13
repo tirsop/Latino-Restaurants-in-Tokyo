b5d658f0881fde25055c81ee3dde650a298fccbe
Tirso Pascual <tpascualgarcia@gmail.com>
2022-04-13 19:58:09 +0900

    add some client-side validation. video 446 minute 4:45 does not work bc form is not according to the model. Let's review the form

--- app.js
+++ app.js
@@ -7,15 +7,17 @@ import { URL } from 'url';
 const __dirname = new URL('.', import.meta.url).pathname;
 import methodOverride from 'method-override';             // for using put/patch request in the html forms
 import ejsMate from 'ejs-mate';
-import mongoose from "mongoose";
+import catchAsync from './utils/catchAsync.js';            // try and catch errors in async functions
+import ExpressError from './utils/ExpressError.js';        // throw an error with custome statusCode and msg
+import mongoose from 'mongoose';
 mongoose.connect('mongodb://localhost:27017/latinoRestaurants')
-    .then(() => {
-        console.log(`--------------console.log\nDatabase connected\n`)
-    })
-    .catch(err => {
-        console.log(`--------------console.log\nMONGO CONNECTION ERROR:`)
-        console.log(err + `\n`)
-    })
+  .then(() => {
+    console.log(`--------------console.log\nDatabase connected\n`)
+  })
+  .catch(err => {
+    console.log(`--------------console.log\nMONGO CONNECTION ERROR:`)
+    console.log(err + `\n`)
+  })
 import Restaurant from './models/restaurant.js';                // import mongoose model created inside models folder
 
 const app = express();                                          // abbreviation of the code
@@ -30,45 +32,49 @@ app.use(methodOverride('_method'));                       // to use PUT, PATCH, 
 
 // Index page, list of all items. 
 app.get('/restaurants', async (req, res) => {
-    const restaurants = await Restaurant.find({});
-    res.render('restaurants/index', { restaurants });
+  const restaurants = await Restaurant.find({});
+  res.render('restaurants/index', { restaurants });
 })
 // Creates new item. 1st display a form (/new) and then post request to save the item (req.body) in the db
 app.get('/restaurants/new', (req, res) => {
-    res.render('restaurants/new');
+  res.render('restaurants/new');
 })
-app.post('/restaurants', async (req, res) => {
-    const restaurant = new Restaurant(req.body.restaurant);
-    await restaurant.save();
-    res.redirect(`/restaurants/${restaurant._id}`);
-})
+app.post('/restaurants', catchAsync(async (req, res) => {
+  const restaurant = new Restaurant(req.body.restaurant);
+  await restaurant.save();
+  res.redirect(`/restaurants/${restaurant._id}`);
+}))
 // Show the details of each item finding them by id
 app.get('/restaurants/:id', async (req, res) => {
-    const { id } = req.params;
-    const restaurant = await Restaurant.findById(id);
-    res.render('restaurants/show', { restaurant });
+  const { id } = req.params;
+  const restaurant = await Restaurant.findById(id);
+  res.render('restaurants/show', { restaurant });
 })
 // Updates/edit item. 1st shows pre-filled form, then updates value & redirect to show-details page.
 app.get('/restaurants/:id/edit', async (req, res) => {
-    const { id } = req.params;
-    const restaurant = await Restaurant.findById(id);
-    res.render('restaurants/edit', { restaurant });
+  const { id } = req.params;
+  const restaurant = await Restaurant.findById(id);
+  res.render('restaurants/edit', { restaurant });
 })
 app.put('/restaurants/:id', async (req, res) => {
-    const { id } = req.params;
-    const restaurant = await Restaurant.findByIdAndUpdate(id, { ...req.body.restaurant });
-    res.redirect(`/restaurants/${restaurant._id}`);
+  const { id } = req.params;
+  const restaurant = await Restaurant.findByIdAndUpdate(id, { ...req.body.restaurant });
+  res.redirect(`/restaurants/${restaurant._id}`);
 })
 // Delete an item by taking its id. 'Delete'button is at the show.ejs
 app.delete('/restaurants/:id', async (req, res) => {
-    const { id } = req.params;
-    await Restaurant.findByIdAndDelete(id);
-    res.redirect('/restaurants');
+  const { id } = req.params;
+  await Restaurant.findByIdAndDelete(id);
+  res.redirect('/restaurants');
 })
 
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = 'Oh no, Something went wrong!'
+  res.status(statusCode).render('error', { err });
+})
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`--------------console.log\nListening at:\nhttp://localhost:${port}\n`);
+  console.log(`--------------console.log\nListening at:\nhttp://localhost:${port}\n`);
 })
-
--- models/restaurant.js
+++ models/restaurant.js
@@ -1,23 +1,23 @@
 import mongoose from "mongoose";
 
 const RestaurantSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    location: {
-        type: String,
-        required: true
-    },
-    country: {
-        type: String,
-        required: true
-    },
-    url: {
-        type: String,
-        required: true
-    },
-    image: String
+  name: {
+    type: String,
+    // required: true  // not need to require bc there's client-side validations with bootstrap and server-side validations
+  },
+  location: {
+    type: String,
+    // required: true
+  },
+  country: {
+    type: String,
+    // required: true
+  },
+  url: {
+    type: String,
+    // required: true
+  },
+  image: String
 })
 
 export default mongoose.model('Restaurant', RestaurantSchema); 
